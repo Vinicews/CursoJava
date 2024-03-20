380ae3897c1edf3469ce687efd5536682af5e5ce
Vinicews <carvalho.vinicius2@gmail.com>
2024-03-20 13:32:03 -0300

    Primeiro commit

---
+++ src/classe/DataTeste.java
@@ -0,0 +1,35 @@
+package classe;
+
+public class DataTeste {
+	
+	public static void main(String[] args) {
+		
+		
+		Data d1 = new Data();
+		d1.dia=06;
+		d1.mes=06;
+		d1.ano=1994;
+		
+		
+		
+		
+		Data d2 = new Data();
+		d2.dia=02;
+		d2.mes=06;
+		d2.ano=1995;
+		
+		
+		System.out.println("Primeira data: " + d1.dia + "/"
+		+ d1.mes + "/" + d1.ano);
+		System.out.println("Segunda data: " + d2.dia + "/"
+				+ d2.mes + "/" + d2.ano);
+	
+		
+		
+		
+		
+		
+	}
+	
+
+}
