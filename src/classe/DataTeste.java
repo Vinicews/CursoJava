2e149e3a3e7be6146bdcca438931be901d1fc6d3
Vinicews <carvalho.vinicius2@gmail.com>
2024-03-20 21:01:17 -0300

    Atualizar módulo do curso ClasseVsMetodos

--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -13,16 +13,17 @@ public class DataTeste {
 		
 		
 		
-		Data d2 = new Data();
+		var d2 = new Data();
 		d2.dia=02;
 		d2.mes=06;
 		d2.ano=1995;
 		
 		
-		System.out.println("Primeira data: " + d1.dia + "/"
-		+ d1.mes + "/" + d1.ano);
-		System.out.println("Segunda data: " + d2.dia + "/"
-				+ d2.mes + "/" + d2.ano);
+		System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
+		
+		System.out.printf("%d/%d/%d", d2.dia, d2.mes, d2.ano);
+		
+		
 	
 		
 		
--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -6,27 +6,24 @@ public class ProdutoTeste {
 	public static void main(String[] args) {
 		
 		Produto p1 = new Produto();
-		p1.nome="Notebook";
-		p1.preco=4356.89;
-		p1.desconto=0.25;
-		
-		
-		
+		p1.nome= "Notebook";
+		p1.preco = 4500.00;
+		p1.desconto = 0.10;
+		double precoFinal1 = p1.precoComDesconto(0.10);		
 		
 		var p2 = new Produto();
-		p2.nome = "Caneta";
-		p2.preco=12.56;
-		p2.desconto=0.29;
-				
+		p2.nome= "Caixa de Som";
+		p2.preco = 50.00;
+		p2.desconto = 0.10;
+		double precoFinal2 = p2.precoComDesconto();		
+		
 		System.out.println(p1.nome);
+		System.out.println("O preço com desconto é: " + precoFinal1 );
 		System.out.println(p2.nome);
+		System.out.println("O preço com desconto é: " + precoFinal2 );
 		
 		
-		double precoFinal = p1.preco * (1 - p1.desconto);
-		double precoFinal1 = p2.preco * (1 - p2.desconto);
-		System.out.println("O preço final é:  " + precoFinal);
-		System.out.println("O preço final é: " + precoFinal1);
-		System.out.println("O total da compras foi: " + (precoFinal1 + precoFinal));
+		
 
 
    }
