22a5fd0e03ca83719445a496fd38233207037937
Vinicews <carvalho.vinicius2@gmail.com>
2024-03-22 12:11:59 -0300

    Committing local changes before pulling remote changes

--- src/classe/AreaCirc.java
+++ src/classe/AreaCirc.java
@@ -1,23 +1,21 @@
 package classe;
 
 public class AreaCirc {
-	
-	
+
 	double raio;
-	static final double  PI = 3.14;
-	
-	
-	
-	
-  AreaCirc(double raioInicial) {
-	raio = raioInicial;
- }
+	static final double PI = 3.14;
 
-   double  area() {
-	return PI * Math.pow(raio, 2);
-	
-	
-}
+	AreaCirc(double raioInicial) {
+		raio = raioInicial;
+	}
 
+	double area() {
+		return PI * Math.pow(raio, 2);
 
+	}
+
+	static double area(double raio) {
+		return PI * Math.pow(raio, 2);
+
+	}
 }
